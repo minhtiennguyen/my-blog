d328420571756f1801342ad01a25290df2f718c4
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-27 14:17:15 +1100

    add pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
     <Router>
       <div className='App'>
-        <Route path='/' exact component={HomePage} />
+        <div id='page-body'>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/articles-list' exact component={ArticlesList} />
+          <Route path='/article' exact component={ArticlePage} />
+        </div>
       </div>
     </Router>
   );
